f6a65ab4f304efbbf4e58c0e5a3eb290f7f8d315
Hanqer <37238621+Hanqer@users.noreply.github.com>
2018-09-06 20:43:06 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,80 +1,75 @@
 from socket import *
 from threading import Thread
 from time import sleep
-IP = '10.190.86.194'
-Host,Port = 'localhost',8888 
-def getJson():
-    with open('h.json','r') as f:
-        return f.read()
-def handleConnection(newsocket,addr):
+from urllib import parse,request
+import time
+import json
+from sql import *
 
-    # while True:
-    information = newsocket.recv(1024)
-    if len(information) > 0:
-        print(str(information.decode('utf-8')))
-        newsocket.sendall(getJson().encode('utf-8'))
-    #print(len(information))
-         # print(str(information.decode('utf-8')))
+
+Host,Port = 'localhost', 7007 
+
+cache_time = {}
+cache_data = {}
+
+def validateDate(str):
+    data = json.loads(str)
+    if data['resultcode'] != '200':
+        return False
+    return True
+
+def getData(city):
+    if city in cache_time.keys():
+        if cache_time[city] + 600 >= time.time():
+            return cache_data[city]
+    url = 'http://v.juhe.cn/weather/index?'
+    parm = {'format':'1','cityname':rec, 'key':'7dfab88a535f36c04d886aba7cad8e75'}
+    parm = parse.urlencode(parm)
+    url = url + parm
+    res = request.urlopen(url).read().decode('utf-8')
+    if validateDate(res) is True:
+        cache_time[city] = time.time()
+        cache_data[city] = res
+        return True
+    return False
+
+
+
+def handle_request(connection, addr):
+    info = connection.recv(1024)
+    if len(info) > 0:
+        city = str(info.decode())
+
+        connection.sendall('okokok'.encode('utf-8'))
+        
     else:
         print('client was closed')
-    #sleep(10)
-    print('done')
-    newsocket.close()
-def main():
-    serSocket = socket(AF_INET,SOCK_STREAM)
-    serSocket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-    serSocket.bind((Host,Port))
-    serSocket.listen(3)
-    print('Serving HTTP on port %s ...' % Port)
+    connection.close()
 
+
+def Start():
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.bind((Host, Port))
+    sock.listen(10)
     try:
         while True:
-            newsocket,addr = serSocket.accept()
-            print('--success connect---')
-            client = Thread(target=handleConnection,args=(newsocket,addr))
+            connection, address = sock.accept()
+            client = Thread(target=handle_request, args=(connection, address))
             client.start()
     finally:
-        serSocket.close()
-if __name__ == '__main__':
-    main()
-    
-from socket import *
-from threading import Thread
-from time import sleep
-IP = '10.190.86.194'
-Host,Port = 'localhost',8888 
-def getJson():
-    with open('h.json','r') as f:
-        return f.read()
-def handleConnection(newsocket,addr):
+        sock.close()
 
-    # while True:
-    information = newsocket.recv(1024)
-    if len(information) > 0:
-        print(str(information.decode('utf-8')))
-        newsocket.sendall(getJson().encode('utf-8'))
-    #print(len(information))
-         # print(str(information.decode('utf-8')))
-    else:
-        print('client was closed')
-    #sleep(10)
-    print('done')
-    newsocket.close()
-def main():
-    serSocket = socket(AF_INET,SOCK_STREAM)
-    serSocket.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
-    serSocket.bind((Host,Port))
-    serSocket.listen(3)
-    print('Serving HTTP on port %s ...' % Port)
-
-    try:
-        while True:
-            newsocket,addr = serSocket.accept()
-            print('--success connect---')
-            client = Thread(target=handleConnection,args=(newsocket,addr))
-            client.start()
-    finally:
-        serSocket.close()
 if __name__ == '__main__':
-    main()
-    
+    SQL = sqlFactory('root', 'yiersan321', 'test')
+    val = []
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    val.append('\'xian\'')
+    SQL.insertData(val)
+    SQL.close()
